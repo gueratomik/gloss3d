a47e2604939b18d94ed35b8280addf236bf227e5
Gary Gabriel <garybaldi.baldi@laposte.net>
2017-06-05 23:00:23 +0200

    saving views

---
+++ code/g3dui/common/g3duiexport.c
@@ -0,0 +1,165 @@
+/******************************************************************************/
+/*  GLOSS3D is free software: you can redistribute it and/or modify           */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation, either version 3 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  GLOSS3D is distributed in the hope that it will be useful,                */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with GLOSS3D.  If not, see http://www.gnu.org/licenses/.            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*  Copyright: Gary GABRIEL - garybaldi.baldi@laposte.net - 2012-2017         */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/* Please avoid using global variables at all costs in this file, and never   */
+/* forget this :                                                              */
+/*                         Keep It Simple Stupid !                            */
+/*                                                                            */
+/******************************************************************************/
+#include <config.h>
+#include <g3dui.h>
+
+/******************************************************************************/
+uint32_t g3duisettings_blockSize ( G3DUI *gui ) {
+    uint32_t blocksize = 0x00;
+    LIST *ltmpview = gui->lview;
+
+    while ( ltmpview ) {
+        G3DUIVIEW *view = ( G3DUIVIEW * ) ltmpview->data;
+
+        blocksize += common_g3duiview_blockSize ( view ) + 0x06;
+
+        ltmpview = ltmpview->next;
+    }
+
+    return blocksize;
+}
+
+/******************************************************************************/
+void g3duisettings_writeBlock ( G3DUI *gui, 
+                                FILE  *fdst ) {
+    LIST *ltmpview = gui->lview;
+
+    chunk_write ( G3DUISETTINGSSIG, 
+                  g3duisettings_blockSize ( gui ),
+                  fdst );
+
+    while ( ltmpview ) {
+        G3DUIVIEW *view = ( G3DUIVIEW * ) ltmpview->data;
+
+        common_g3duiview_writeBlock ( view, fdst );
+
+        ltmpview = ltmpview->next;
+    }
+}
+
+
+/******************************************************************************/
+void g3duisettings_readBlock ( G3DUI    *gui, 
+                               G3DSCENE *sce,
+                               FILE     *fsrc ) {
+    LIST *ltmpview = gui->lview;
+    G3DUIVIEW *view = NULL;
+    uint16_t chunksig;
+    uint32_t chunklen;
+
+    readf ( &chunksig, sizeof ( uint16_t ), 0x01, fsrc );
+    readf ( &chunklen, sizeof ( uint32_t ), 0x01, fsrc );
+
+    do {
+        switch ( chunksig ) {
+            case G3DUISETTINGSSIG :
+            break;
+
+            case G3DUIVIEWSIG :
+                if ( ltmpview ) {
+                    view = ( G3DUIVIEW * ) ltmpview->data;
+
+                    ltmpview = ltmpview->next;
+                }
+            break;
+
+            case G3DUIVIEWDEFAULTCAMERAPOSITIONSIG :
+                if ( view ) {
+                    readf ( &((G3DOBJECT*)view->defcam)->pos.x, sizeof ( float ), 1, fsrc );
+                    readf ( &((G3DOBJECT*)view->defcam)->pos.y, sizeof ( float ), 1, fsrc );
+                    readf ( &((G3DOBJECT*)view->defcam)->pos.z, sizeof ( float ), 1, fsrc );
+                }
+            break;
+
+            case G3DUIVIEWDEFAULTCAMERAROTATIONSIG :
+                if ( view ) {
+                    readf ( &((G3DOBJECT*)view->defcam)->rot.x, sizeof ( float ), 1, fsrc );
+                    readf ( &((G3DOBJECT*)view->defcam)->rot.y, sizeof ( float ), 1, fsrc );
+                    readf ( &((G3DOBJECT*)view->defcam)->rot.z, sizeof ( float ), 1, fsrc );
+                }
+            break;
+
+            case G3DUIVIEWDEFAULTCAMERASCALINGSIG :
+                if ( view ) {
+                    readf ( &((G3DOBJECT*)view->defcam)->sca.x, sizeof ( float ), 1, fsrc );
+                    readf ( &((G3DOBJECT*)view->defcam)->sca.y, sizeof ( float ), 1, fsrc );
+                    readf ( &((G3DOBJECT*)view->defcam)->sca.z, sizeof ( float ), 1, fsrc );
+                }
+            break;
+
+            case G3DUIVIEWDEFAULTCAMERAFOCALSIG :
+                if ( view ) {
+                    readf ( &view->defcam->focal, sizeof ( float ), 1, fsrc );
+                }
+            break;
+
+            case G3DUIVIEWUSECAMERASIG : {
+                if ( view ) {
+                    LIST *lcam = g3dobject_getChildrenByType ( sce, 
+                                                               G3DCAMERATYPE );
+                    LIST *ltmpcam = lcam;
+                    uint32_t useCameraID;
+
+                    readf ( &useCameraID, sizeof ( uint32_t ), 1, fsrc );
+
+                    while ( ltmpcam ) {
+                        G3DCAMERA *cam = ( G3DCAMERA * ) ltmpcam->data;
+
+                        if ( ((G3DOBJECT*)cam)->id == useCameraID ) {
+                            view->cam = cam;
+                        }
+
+                        ltmpcam = ltmpcam->next;
+                    }
+                }
+            } break;
+
+            case 0xFFFF : { /* EXTENSIONENDSIG */
+                ltmpview = gui->lview;
+
+                while ( ltmpview ) {
+                    G3DUIVIEW *view = ( G3DUIVIEW * ) ltmpview->data;
+
+                    g3dobject_updateMatrix_r ( view->defcam, gui->flags );
+
+                    ltmpview = ltmpview->next;
+                }
+
+                return;
+            } break;
+
+            default :
+                fseek ( fsrc, chunklen, SEEK_CUR );
+            break;
+        }
+
+        readf ( &chunksig, sizeof ( uint16_t ), 0x01, fsrc );
+        readf ( &chunklen, sizeof ( uint32_t ), 0x01, fsrc );
+    } while ( feof ( fsrc ) == 0x00 );
+}
---
+++ code/g3dui/common/g3duiviewexport.c
@@ -0,0 +1,140 @@
+/******************************************************************************/
+/*  GLOSS3D is free software: you can redistribute it and/or modify           */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation, either version 3 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  GLOSS3D is distributed in the hope that it will be useful,                */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with GLOSS3D.  If not, see http://www.gnu.org/licenses/.            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*  Copyright: Gary GABRIEL - garybaldi.baldi@laposte.net - 2012-2017         */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/* Please avoid using global variables at all costs in this file, and never   */
+/* forget this :                                                              */
+/*                         Keep It Simple Stupid !                            */
+/*                                                                            */
+/******************************************************************************/
+#include <config.h>
+#include <g3dui.h>
+
+/******************************************************************************/
+static uint32_t g3duiviewdefaultcameraposition_blockSize ( ) {
+    return sizeof ( float ) * 0x03;
+}
+
+/******************************************************************************/
+static void g3duiviewdefaultcameraposition_writeBlock ( G3DVECTOR *pos, 
+                                                        FILE      *fdst ) {
+    chunk_write ( G3DUIVIEWDEFAULTCAMERAPOSITIONSIG, 
+                  g3duiviewdefaultcameraposition_blockSize ( ),
+                  fdst );
+
+    writef ( &pos->x, sizeof ( float ), 0x01, fdst );
+    writef ( &pos->y, sizeof ( float ), 0x01, fdst );
+    writef ( &pos->z, sizeof ( float ), 0x01, fdst );
+}
+
+/******************************************************************************/
+static uint32_t g3duiviewdefaultcamerarotation_blockSize ( ) {
+    return sizeof ( float ) * 0x03;
+}
+
+/******************************************************************************/
+static void g3duiviewdefaultcamerarotation_writeBlock ( G3DVECTOR *rot, 
+                                                        FILE      *fdst ) {
+    chunk_write ( G3DUIVIEWDEFAULTCAMERAROTATIONSIG, 
+                  g3duiviewdefaultcamerarotation_blockSize ( ),
+                  fdst );
+
+    writef ( &rot->x, sizeof ( float ), 0x01, fdst );
+    writef ( &rot->y, sizeof ( float ), 0x01, fdst );
+    writef ( &rot->z, sizeof ( float ), 0x01, fdst );
+}
+
+/******************************************************************************/
+static uint32_t g3duiviewdefaultcamerascaling_blockSize ( ) {
+    return sizeof ( float ) * 0x03;
+}
+
+/******************************************************************************/
+static void g3duiviewdefaultcamerascaling_writeBlock ( G3DVECTOR *sca, 
+                                                       FILE      *fdst ) {
+    chunk_write ( G3DUIVIEWDEFAULTCAMERASCALINGSIG, 
+                  g3duiviewdefaultcamerascaling_blockSize ( ),
+                  fdst );
+
+    writef ( &sca->x, sizeof ( float ), 0x01, fdst );
+    writef ( &sca->y, sizeof ( float ), 0x01, fdst );
+    writef ( &sca->z, sizeof ( float ), 0x01, fdst );
+}
+
+/******************************************************************************/
+static uint32_t g3duiviewdefaultcamerafocal_blockSize ( ) {
+    return sizeof ( float );
+}
+
+/******************************************************************************/
+static void g3duiviewdefaultcamerafocal_writeBlock ( float  focal, 
+                                                     FILE  *fdst ) {
+    chunk_write ( G3DUIVIEWDEFAULTCAMERAFOCALSIG, 
+                  g3duiviewdefaultcamerafocal_blockSize ( ),
+                  fdst );
+
+    writef ( &focal, sizeof ( float ), 0x01, fdst );
+}
+
+/******************************************************************************/
+static uint32_t g3duiviewusecamera_blockSize ( ) {
+    return sizeof ( uint32_t );
+}
+
+/******************************************************************************/
+static void g3duiviewusecamera_writeBlock ( G3DCAMERA *cam, 
+                                            FILE      *fdst ) {
+    chunk_write ( G3DUIVIEWUSECAMERASIG, 
+                  g3duiviewusecamera_blockSize ( ),
+                  fdst );
+
+    writef ( &((G3DOBJECT*)cam)->id, sizeof ( uint32_t ), 0x01, fdst );
+}
+
+/******************************************************************************/
+uint32_t common_g3duiview_blockSize ( G3DUIVIEW *view ) {
+    uint32_t blocksize = 0x00;
+
+    blocksize += g3duiviewdefaultcameraposition_blockSize ( ) + 0x06;
+    blocksize += g3duiviewdefaultcamerarotation_blockSize ( ) + 0x06;
+    blocksize += g3duiviewdefaultcamerascaling_blockSize ( ) + 0x06;
+    blocksize += g3duiviewdefaultcamerafocal_blockSize ( ) + 0x06;
+    blocksize += g3duiviewusecamera_blockSize ( ) + 0x06;
+
+    return blocksize;
+}
+
+/******************************************************************************/
+void common_g3duiview_writeBlock ( G3DUIVIEW *view, FILE *fdst ) {
+    chunk_write ( G3DUIVIEWSIG, 
+                  common_g3duiview_blockSize ( view ),
+                  fdst );
+
+    g3duiviewdefaultcameraposition_writeBlock ( &((G3DOBJECT*)view->defcam)->pos, fdst );
+    g3duiviewdefaultcamerarotation_writeBlock ( &((G3DOBJECT*)view->defcam)->rot, fdst );
+    g3duiviewdefaultcamerascaling_writeBlock  ( &((G3DOBJECT*)view->defcam)->sca, fdst );
+    g3duiviewdefaultcamerafocal_writeBlock    (  view->defcam->focal, fdst );
+
+    if ( view->cam != view->defcam ) {
+        g3duiviewusecamera_writeBlock ( view->cam, fdst );
+    }
+}
