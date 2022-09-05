191a878586fb88d245f0ff645f16077512e1a457
root <garybaldi.baldi@laposte.net>
2022-09-06 00:21:21 +0200

    refurbishing GUI in progress

---
+++ code/g3dui/gtk3/g3duicom.c
@@ -0,0 +1,170 @@
+/******************************************************************************/
+/*  GLOSS3D is free software: you can redistribute it and/or modify           */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation, either version 3 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  GLOSS3D is distributed in the hope that it will be useful,                */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with GLOSS3D.  If not, see http://www.gnu.org/licenses/.            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*  Copyright: Gary GABRIEL - garybaldi.baldi@laposte.net - 2012-2020         */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/* Please avoid using global variables at all costs in this file, and never   */
+/* forget this :                                                              */
+/*                         Keep It Simple Stupid !                            */
+/*                                                                            */
+/******************************************************************************/
+#include <config.h>
+#include <g3dui_gtk3.h>
+
+/******************************************************************************/
+static void gotoframe ( GTK3G3DUI *gtk3gui,
+                        GOTOFRAME *gtf ) {
+    G3DUI *gui = &gtk3gui->core;
+
+    /*** Force disabling real time subdivision ***/
+    /*** otherwise g3dobject_anim_r() may call some buffered subdivision ***/
+    /*** updates and that;s gonna be really slow. We prefer to rely ***/
+    /*** On real time stuff ***/
+    gui->engine_flags |= ONGOINGANIMATION;
+/*printf("g3dgotoframe %s %f\n", __func__, gtf->frame );*/
+
+    g3dobject_anim_r ( ( G3DOBJECT * ) gui->sce, 
+                                       gtf->frame, 
+                                       gui->engine_flags |
+                                       gtf->extra_engine_flags );
+
+    g3dobject_update_r ( ( G3DOBJECT * ) gui->sce, gui->engine_flags |
+                                                   gtf->extra_engine_flags );
+
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWVIEW | REDRAWTIMELINE );
+
+    gui->curframe = gtf->frame;
+
+    g3dui_processAnimatedImages ( gui );
+
+    /*** Re-enable real time subdivision ***/
+    gui->engine_flags &= (~ONGOINGANIMATION);
+}
+
+/******************************************************************************/
+static void dumpscreen ( GTK3G3DUI  *gtk3gui, 
+                         DUMPSCREEN *dsn ) {
+    G3DUI *gui = &gtk3gui->core;
+
+    glReadPixels ( dsn->x, 
+                   dsn->y, 
+                   dsn->width,
+                   dsn->height, 
+                   GL_RGBA, GL_UNSIGNED_BYTE,
+                   dsn->buffer );
+}
+
+/****************** Event handler for interprocess communication **************/
+void g3duicom_handleAction ( GtkWidget *widget, 
+                             gpointer   ptr, 
+                             gpointer   user_data ) {
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) user_data;
+    G3DUIACTION *action = ( G3DUIACTION * ) ptr;
+    G3DUI *gui = &gtk3gui->core;
+
+    switch ( action->type ) {
+        case ACTION_GOTOFRAME :
+            gotoframe ( gui, action );
+        break;
+
+        case ACTION_DUMPSCREEN :
+            dumpscreen ( gui, action );
+        break;
+
+        default:
+        break;
+    }
+
+    pthread_mutex_unlock ( &action->done );
+}
+
+/******************************************************************************/
+static gboolean emitAction ( G3DUIACTION *action ) {
+    GTK3G3DUI *gtk3gui = action->gui;
+#ifdef TODO
+    g_signal_emit_by_name ( gtk3gui->main, "action", action );
+#endif
+
+    return G_SOURCE_REMOVE;
+}
+
+/******************************************************************************/
+void g3duicom_requestActionFromMainThread ( GTK3G3DUI   *gtk3gui,
+                                            G3DUIACTION *action ) {
+    G3DUI *gui = &gtk3gui->core;
+
+    pthread_mutex_lock(&action->done);
+
+    /*** Because OpenGL only works within the main context, we ***/
+    /*** cannot call g_signal_emit_by_name() directly, we have to ***/
+    /*** call g_main_context_invoke_full() that calls a wrapper to ***/
+    /*** g_signal_emit_by_name() ***/
+    g_main_context_invoke_full ( NULL,
+#ifdef __linux__
+                                 G_PRIORITY_HIGH, 
+#endif
+#ifdef __MINGW32__
+                                 G_PRIORITY_DEFAULT,
+#endif
+                    (GSourceFunc)emitAction,
+                                 action,
+                                 NULL );
+
+    /** wait until completion **/
+    pthread_mutex_lock    ( &action->done );
+    pthread_mutex_unlock  ( &action->done );
+}
+
+/******************************************************************************/
+uint32_t filtergotoframe_draw ( Q3DFILTER     *fil, 
+                                Q3DJOB        *qjob,
+                                uint32_t       cpuID, 
+                                float          frameID,
+                                unsigned char *img, 
+                                uint32_t       from, 
+                                uint32_t       to, 
+                                uint32_t       depth, 
+                                uint32_t       width ) {
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) fil->data;
+    static GOTOFRAME gtf = { .action = { .done = PTHREAD_MUTEX_INITIALIZER } };
+
+    gtf.action.type        =  ACTION_GOTOFRAME;
+    gtf.action.gui         = &gtk3gui->core;
+    gtf.frame              =  frameID;
+    gtf.extra_engine_flags =  ONGOINGRENDERING;
+
+    g3duicom_requestActionFromMainThread ( gtk3gui, &gtf );
+
+    return 0x00;
+}
+
+/******************************************************************************/
+Q3DFILTER *q3dfilter_gotoframe_new ( GTK3G3DUI *gtk3gui ) {
+    Q3DFILTER *fil;
+
+    fil = q3dfilter_new ( FILTERIMAGE, GOTOFRAMEFILTERNAME,
+                                       filtergotoframe_draw,
+                                       NULL,
+                                       gtk3gui );
+
+    return fil;
+}
---
+++ code/g3dui/gtk3/g3duitimeboard.c
@@ -0,0 +1,444 @@
+/******************************************************************************/
+/*  GLOSS3D is free software: you can redistribute it and/or modify           */
+/*  it under the terms of the GNU General Public License as published by      */
+/*  the Free Software Foundation, either version 3 of the License, or         */
+/*  (at your option) any later version.                                       */
+/*                                                                            */
+/*  GLOSS3D is distributed in the hope that it will be useful,                */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/*  GNU General Public License for more details.                              */
+/*                                                                            */
+/*  You should have received a copy of the GNU General Public License         */
+/*  along with GLOSS3D.  If not, see http://www.gnu.org/licenses/.            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*  Copyright: Gary GABRIEL - garybaldi.baldi@laposte.net - 2012-2020         */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/* Please avoid using global variables at all costs in this file, and never   */
+/* forget this :                                                              */
+/*                         Keep It Simple Stupid !                            */
+/*                                                                            */
+/******************************************************************************/
+#include <config.h>
+#include <g3dui_gtk3.h>
+
+/******************************************************************************/
+#include <xpm/record.xpm>
+#include <xpm/stopanim.xpm>
+#include <xpm/playanim.xpm>
+#include <xpm/prevframe.xpm>
+#include <xpm/nextframe.xpm>
+#include <xpm/zoomtime.xpm>
+#include <xpm/unzoomtime.xpm>
+#include <xpm/scale_keys.xpm>
+#include <xpm/pick_keys.xpm>
+
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+static GTK3G3DUITIMEBOARD *gtk3_g3duitimeboard_new ( GTK3G3DUI *gtk3gui ) {
+    uint32_t memSize =  sizeof ( GTK3G3DUITIMEBOARD );
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) calloc ( 0x01, memSize );
+
+    if ( gtk3timeboard == NULL ) {
+        fprintf ( stderr, "%s: calloc failed\n", __func__ );
+
+        return NULL;
+
+    }
+
+    gtk3timeboard->core.gui = ( G3DUI * ) gtk3gui;
+
+
+    return gtk3timeboard;
+}
+
+/******************************************************************************/
+void gtk3_g3duitimeboard_resize ( GTK3G3DUITIMEBOARD *gtk3timeboard,
+                                  uint32_t              width,
+                                  uint32_t              height ) {
+    GdkRectangle gdkrec;
+
+    g3duitimeboard_resize ( &gtk3timeboard->core,
+                             width,
+                             height );
+
+    if ( gtk3timeboard->buttonsFixed ) {
+        g3duirectangle_toGdkRectangle ( &gtk3timeboard->core.btnrec, &gdkrec );
+
+        gtk_layout_move ( gtk3timeboard->layout,
+               GTK_WIDGET(gtk3timeboard->buttonsFixed),
+                          gdkrec.x,
+                          gdkrec.y );
+
+        gtk_widget_set_size_request ( GTK_WIDGET(gtk3timeboard->buttonsFixed),
+                                      gdkrec.width,
+                                      gdkrec.height );
+    }
+
+    /*** Timeline ***/
+    /*if ( gtk3timeboard->core.timeline ) {
+        GTK3G3DUITIMELINE *gtk3timeline = ( GTK3G3DUITIMELINE * )
+                                           gtk3timeboard->core.timeline;
+
+        g3duirectangle_toGdkRectangle ( &gtk3timeboard->core.linerec, &gdkrec );
+
+        gtk_layout_move ( gtk3timeboard->layout,
+               GTK_WIDGET(gtk3timeline->area), 
+                          gdkrec.x,
+                          gdkrec.y );
+
+        gtk_widget_set_size_request ( GTK_WIDGET(gtk3timeline->area), 
+                                      gdkrec.width,
+                                      gdkrec.height  );
+    }*/
+
+
+    if ( gtk3timeboard->functionsFixed ) {
+        g3duirectangle_toGdkRectangle ( &gtk3timeboard->core.funcrec, &gdkrec );
+
+        gtk_layout_move ( gtk3timeboard->layout,
+              GTK_WIDGET(gtk3timeboard->functionsFixed),
+                          gdkrec.x,
+                          gdkrec.y );
+
+        gtk_widget_set_size_request ( GTK_WIDGET(gtk3timeboard->functionsFixed),
+                                      gdkrec.width,
+                                      gdkrec.height );
+    }
+}
+
+/******************************************************************************/
+/*static void createTimeline ( GTK3G3DUITIMEBOARD *gtk3timeboard ) {
+    GTK3G3DUIOBJECTEDIT *gtk3objedit;
+
+    gtk3objedit = gtk3_g3duiobjectedit_create ( gtk3timeboard->layout,
+                                ( GTK3G3DUI * ) gtk3timeboard->core.gui,
+                                                "Current object" );
+
+    gtk_layout_put ( GTK_LAYOUT(gtk3timeboard->layout), gtk3objedit->scrolled, 0, 0 );
+
+    gtk3timeboard->core.objedit = ( G3DUIOBJECTEDIT * ) gtk3objedit;
+}*/
+
+/******************************************************************************/
+static void useMoveToolCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeline_setTool ( gtk3timeboard->core.timeline, TIME_MOVE_TOOL );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWTIMELINE );
+}
+
+/******************************************************************************/
+static void usePanToolCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeline_setTool ( gtk3timeboard->core.timeline, TIME_PAN_TOOL );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWTIMELINE );
+}
+
+/******************************************************************************/
+static void useScaleToolCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeline_setTool ( gtk3timeboard->core.timeline, TIME_SCALE_TOOL );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWTIMELINE );
+}
+
+/******************************************************************************/
+static void createFunctions ( GTK3G3DUITIMEBOARD *gtk3timeboard ) {
+    GtkFixed *fixed = ui_gtk_fixed_new ( CLASS_MAIN );
+
+    gtk3timeboard->functionsFixed = fixed;
+
+    gtk_widget_set_size_request ( GTK_WIDGET(fixed), 48, 24 );
+
+    gtk_layout_put ( GTK_LAYOUT(gtk3timeboard->layout), 
+                     GTK_WIDGET(fixed), 
+                     0, 0 );
+
+
+    gtk3timeboard->scaleButton = ui_createImageButton ( fixed,
+                                                              gtk3timeboard,
+                                                              TIMEBOARDRECORD,
+                                                              CLASS_MAIN,
+                                                              scale_keys_xpm,
+                                                              0,  0, 24, 24,
+                                                              useScaleToolCbk );
+
+    gtk3timeboard->panButton = ui_createImageButton ( fixed,
+                                                            gtk3timeboard,
+                                                            TIMEBOARDRECORD,
+                                                            CLASS_MAIN,
+                                                            pick_keys_xpm,
+                                                            24,  0, 24, 24,
+                                                            usePanToolCbk );
+
+    gtk_widget_show_all ( GTK_WIDGET(fixed) );
+}
+
+/******************************************************************************/
+static void stopCbk ( GtkWidget *widget,
+                      gpointer   user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeboard_stopCbk ( &gtk3timeboard->core );
+}
+
+/******************************************************************************/
+static void *play_t ( void *user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+    G3DUI *gui = gtk3timeboard->core.gui;
+    G3DSCENE *sce = gui->sce;
+
+    if ( sce ) {
+        float curframe = gui->curframe;
+
+        /*** http://www.motifdeveloper.com/tips/tip6.html ***/
+        while ( gui->playLock ) {
+	        curframe += 1.0f;
+
+            /*** skip sending event if animation has not completed yet ***/
+            if ( ( gui->engine_flags & ONGOINGANIMATION ) == 0x00 ) {
+                static GOTOFRAME gtf;
+
+                gtf.action.type = ACTION_GOTOFRAME;
+                gtf.action.gui  = gui;
+                gtf.frame       = curframe;
+
+                g3duicom_requestActionFromMainThread ( gui, &gtf );
+            }
+
+#ifdef __linux__
+            /*** wow, is this standard ? I had never used such statement style ***/
+            /*** BTW, libGL crashes when it goes to fast, this call is required ***/
+            nanosleep((struct timespec[]){{0, 40000000}}, NULL);
+#endif
+#ifdef  __MINGW32__
+            Sleep ( 40);
+#endif
+        }
+
+#ifdef __linux__
+        pthread_exit ( NULL );
+#endif
+    }
+
+    return NULL;
+}
+
+
+/******************************************************************************/
+static void playCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+    G3DUI *gui = gtk3timeboard->core.gui;
+
+    #ifdef __MINGW32__
+    HANDLE     hdl;
+    #endif 
+
+    /*** if scene is not currently played ***/
+    if ( gui->playLock /*gui->playthreadid*/ == 0x00 ) {
+        gui->playLock = 0x01;
+        #ifdef __linux__
+        pthread_attr_t attr;
+
+        pthread_attr_init ( &attr );
+
+        /*** launch rays in a thread ***/
+        pthread_create ( &gui->playthreadid, &attr, play_t, gui );
+
+        /*** prepare to release resources after thread termination ***/
+        pthread_detach ( gui->playthreadid );
+        #endif
+
+        #ifdef __MINGW32__
+        hdl = CreateThread ( NULL, 
+                             0,
+                             (LPTHREAD_START_ROUTINE) play_t, 
+                             gui,
+                             0,
+                             &gui->playthreadid );
+        #endif
+    }
+
+    /*common_g3dui_playCbk ( gui );*/
+}
+
+/******************************************************************************/
+static void nextFrameCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+    G3DUI *gui = gtk3timeboard->core.gui;
+
+    gui->curframe += 1;
+
+    g3duitimeboard_gotoFrameCbk ( &gtk3timeboard->core );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWVIEW );
+}
+
+/******************************************************************************/
+static void prevFrameCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+    G3DUI *gui = gtk3timeboard->core.gui;
+
+    gui->curframe -= 1;
+
+    g3duitimeboard_gotoFrameCbk ( &gtk3timeboard->core );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWVIEW );
+}
+
+/******************************************************************************/
+static void zoomFrameCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeline_incZoom ( gtk3timeboard->core.timeline, 0x02 );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWTIMELINE );
+}
+
+/******************************************************************************/
+static void unzoomFrameCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeline_decZoom ( gtk3timeboard->core.timeline, 0x02 );
+
+    gtk3_interpretUIReturnFlags ( gtk3gui, REDRAWTIMELINE );
+}
+
+/******************************************************************************/
+static void recordFrameCbk ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+    GTK3G3DUI *gtk3gui = ( GTK3G3DUI * ) gtk3timeboard->core.gui;
+
+    g3duitimeboard_recordFrameCbk ( &gtk3timeboard->core, 
+                                     KEYPOSITION | 
+                                     KEYROTATION | 
+                                     KEYSCALING  /*|
+                                     KEYDATA*/ );
+}
+
+/******************************************************************************/
+static void createButtons ( GTK3G3DUITIMEBOARD *gtk3timeboard ) {
+    GtkFixed *fixed = ui_gtk_fixed_new ( CLASS_MAIN );
+
+    gtk3timeboard->buttonsFixed = fixed;
+
+    gtk_widget_set_size_request ( GTK_WIDGET(fixed), 144, 24 );
+
+    gtk_layout_put ( GTK_LAYOUT(gtk3timeboard->layout),
+                     GTK_WIDGET(fixed),
+                     0, 0 );
+
+    ui_createImageButton ( fixed,
+                           gtk3timeboard,
+                           TIMEBOARDPREV,
+                           CLASS_MAIN,
+                           prevframe_xpm,
+                           0,  0, 24, 24,
+                           prevFrameCbk );
+
+    ui_createImageButton ( fixed,
+                           gtk3timeboard,
+                           TIMEBOARDPLAY,
+                           CLASS_MAIN,
+                           playanim_xpm,
+                           24,  0, 24, 24,
+                           playCbk );
+
+    ui_createImageButton ( fixed,
+                           gtk3timeboard,
+                           TIMEBOARDNEXT,
+                           CLASS_MAIN,
+                           nextframe_xpm,
+                           48,  0, 24, 24,
+                           nextFrameCbk );
+
+    ui_createImageButton ( fixed,
+                           gtk3timeboard,
+                           TIMEBOARDZOOM,
+                           CLASS_MAIN,
+                           zoomtime_xpm,
+                           72,  0, 24, 24,
+                           zoomFrameCbk );
+
+    ui_createImageButton ( fixed,
+                           gtk3timeboard,
+                           TIMEBOARDUNZOOM,
+                           CLASS_MAIN,
+                           unzoomtime_xpm,
+                           96,  0, 24, 24,
+                           unzoomFrameCbk );
+
+    ui_createImageButton ( fixed,
+                           gtk3timeboard,
+                           TIMEBOARDRECORD,
+                           CLASS_MAIN,
+                           record_xpm,
+                           120,  0, 24, 24,
+                           recordFrameCbk );
+
+    gtk_widget_show_all ( GTK_WIDGET(fixed) );
+}
+
+/******************************************************************************/
+static void Destroy ( GtkWidget *widget, gpointer user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+
+    free ( gtk3timeboard );
+}
+
+/******************************************************************************/
+static void Realize ( GtkWidget *widget, 
+                      gpointer   user_data ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = ( GTK3G3DUITIMEBOARD * ) user_data;
+
+
+}
+
+/******************************************************************************/
+GTK3G3DUITIMEBOARD *gtk3_g3duitimeboard_create ( GtkWidget *parent,
+                                                 GTK3G3DUI *gtk3gui,
+                                                 char      *name ) {
+    GTK3G3DUITIMEBOARD *gtk3timeboard = gtk3_g3duitimeboard_new ( gtk3gui );
+    GtkWidget    *layout = gtk_layout_new ( NULL, NULL );
+
+    gtk3timeboard->layout = GTK_LAYOUT(layout);
+
+    gtk_widget_set_name ( layout, name );
+
+    /*gtk_widget_add_events(GTK_WIDGET(layout), GDK_CONFIGURE);*/
+
+    g_signal_connect ( G_OBJECT (layout), "realize", G_CALLBACK (Realize), gtk3timeboard );
+    g_signal_connect ( G_OBJECT (layout), "destroy", G_CALLBACK (Destroy), gtk3timeboard );
+
+    gtk_widget_show ( layout );
+
+    createButtons   ( gtk3timeboard );
+    createFunctions ( gtk3timeboard );
+
+    return gtk3timeboard;
+}
